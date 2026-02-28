e17f14735d75e6eebcd84dad647d4b4823806869
quangthoIT <quangthoIT04@gmail.com>
2026-02-28 20:32:47 +0700

    Them mot so  Page de test

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./layout/Layout";
+import HomePage from "./pages/HomePage";
+import ProductsPage from "./pages/ProductsPage";
+import Featured from "./pages/Featured";
+import LoginPage from "./pages/LoginPage";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/login" element={<LoginPage />} />
         <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />} />
+          <Route path="/products" element={<ProductsPage />} />
+          <Route path="/featured" element={<Featured />} />
         </Route>
       </Routes>
     </BrowserRouter>
