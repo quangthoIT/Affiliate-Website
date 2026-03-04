1f5590751a9f84a78966c362b20ee4ddd1456854
quangthoIT <quangthoIT04@gmail.com>
2026-03-04 19:37:17 +0700

    Fix San pham moi nhat bi nguoc

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,10 +1,7 @@
 import mongoose from "mongoose";
 export const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.DATABASE_CONNECTION_STRING),{
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }
+    await mongoose.connect(process.env.DATABASE_CONNECTION_STRING);
     console.log("MongoDB connected successfully");
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="bg-blue-800 text-gray-200 text-sm italic text-center py-2">
+    <div className="bg-blue-800 text-gray-200 text-xs md:text-sm italic text-center py-2">
       Copyright &copy; 2026 Affiliate Website by Nguyen Quang Tho
     </div>
   );
